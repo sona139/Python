21fa9163ae01db186e111dea53d4025e6bee1c0a
sona <89957853+sona139@users.noreply.github.com>
2022-08-17 00:27:31 +0700

    Delete ICPC0107.py

--- ICPC0107.py
+++
@@ -1,6 +0,0 @@
-for case in range(int(input())):
-    n, m = input().split()
-    str1, str2 = input(), input()
-    num1 = int(str1.replace(n, m)) + int(str2.replace(n, m))
-    num2 = int(str1.replace(m, n)) + int(str2.replace(m, n))
-    print(min(num1, num2), max(num1, num2))
