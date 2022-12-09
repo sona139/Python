eff0ba51b7e408ffbd01038f98546ff4c48e3460
sona <89957853+sona139@users.noreply.github.com>
2022-12-09 22:24:52 +0700

    Update PY01048.py

--- PY01048.py
+++ PY01048.py
@@ -2,8 +2,5 @@ from math import sqrt
 
 for _ in range(int(input())):
     n = int(input())*2
-    res = 0
-    for i in range(2, int(sqrt(n))+1):
-        if n%i == 0 and (n//i - i)&1:
-            res += 1
+    res = sum([i if n%i == 0 and (n//i-i)&1 else 0 for i in range(2, int(sqrt(n))+1)])
     print(res)
